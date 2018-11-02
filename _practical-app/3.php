649cf06732f3ed1be92feb3a2e2de947dff2457c
Bernie Cabase <cabasebernie91@gmail.com>
2018-11-03 00:57:34 +0800

    exercise 3 answered.

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -24,7 +24,37 @@
 	Step 3 : Make a switch Statement that test againts one condition with 5 cases
 
  */
+	if (false) {
+		echo "this will not appear";
+	}else {
+		echo "this will appear";
+	}
+	echo "<br>";
+	for ($i=0; $i < 10 ; $i++) { 
+		echo $i."<br>";
+	}
+	$val = 4;
+	switch ($val) {
+		case 1:
+			echo "selected $val";
+			break;
+		case 2:
+		echo "selected $val";
+			break;
 
+		case 3:
+		echo "selected $val";
+			break;
+		case 4:
+		echo "selected $val";
+			break;
+		case 5:
+		echo "selected $val";
+			break;
+		default:
+		echo "selected $val";
+			break;
+	}
 	
 ?>
 
