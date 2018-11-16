99ea304712400dbe2229cd68f2aa05bd74c46ec8
Bernie Cabase <cabasebernie91@gmail.com>
2018-11-16 22:50:23 +0800

    Committed be bernz - data

---
+++ cms/includes/post_query.php
@@ -0,0 +1,10 @@
+<?php
+    $query = "SELECT * FROM posts";
+    $posts = mysqli_query($connection, $query);
+ 
+    // foreach ($result as $key => $value) {
+    //     $post = [
+    //         $key=>$value
+    //     ];
+    // }
+?>
--- cms/index.php
+++ cms/index.php
@@ -1,4 +1,7 @@
-<?php include "includes/header.php"; ?>
+<?php 
+include "includes/header.php"; 
+include "includes/post_query.php";
+?>
 
 
     <!-- Navigation -->
@@ -13,28 +16,34 @@
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+            <?php
+            
+            while ($post = mysqli_fetch_assoc($posts)) {
+            
+            ?>
+
                 <h1 class="page-header">
-                    Page Heading
+                    <?php echo "Page Heading" ?>
                     <small>Secondary Text</small>
                 </h1>
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#"><?php echo $post["post_title"]; ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php"><?php echo $post["post_author"];?></a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> <?php echo $post["post_date"];?></p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $post["post_content"];?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
-
+            <?php  } ?>
                 <!-- Pager -->
                 <ul class="pager">
                     <li class="previous">
